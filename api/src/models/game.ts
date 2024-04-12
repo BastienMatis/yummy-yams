165ee2cd40d5675fc3a7a2b5bc26967ee9cdcf8b
Bastien Matis <matis.bastien@outlook.fr>
2024-04-12 10:30:28 +0200

    models

---
+++ api/src/models/game.ts
@@ -0,0 +1,45 @@
+import mongoose, { Schema } from "mongoose";
+import { gameKinds } from "../constants/game.constants";
+
+const turnSchema = new Schema(
+    {
+        turn: {
+            type: String,
+            enum: Object.values(gameKinds),
+        },
+        user: {
+            _id: {
+                type: Schema.Types.ObjectId,
+                ref: "User",
+                required: true,
+            },
+        }
+    }
+)
+
+const gameSchema = new Schema(
+    {
+        score: {
+            type: Number,
+            required: true,
+        },
+        turn: {
+            type: [turnSchema],
+            description: 'Le tour du joueur qui dans le front devra se lock à partir de 3'
+        },
+        isYams: {
+            type: Boolean,
+            required: true
+        },
+        isCarre: {
+            type: Boolean,
+            required: true
+        },
+        isDouble: {
+            type: Boolean,
+            required: true
+        },
+    },
+);
+
+export const userModel = mongoose.model("Game", gameSchema, "games");
---
+++ api/src/models/user.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      },
+  },
+);
+
+export const userModel = mongoose.model("User", userSchema, "users");
